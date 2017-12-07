afd69f572eace53b8da95d0a3b1aa4bacbcf2772
Antonio Vivace <puma95.95@gmail.com>
2017-12-07 22:45:13 +0100

    Update Game schema to comply with draft1 json schema specification (dmg01/database)

--- app/models/game.js
+++ app/models/game.js
@@ -1,20 +1,29 @@
 // load the things we need
 var mongoose = require('mongoose');
 
-// define the schema for our user model
 var gameSchema = mongoose.Schema({
     data             : {
+        // Required Fields
         title        : String,
-        permalink    : String,
-        developer    : String, // Should point to user?
+        permalink    : String, // A.K.A. slug
+        developer    : String, // Should(can) point to user
         typetag      : String,
-        description  : String,
         platform     : String,
-        repository   : String,
-        license      : String,
-        tags         : [String],
         rom          : String,
         screenshots  : [String],
+
+        // Optional Fields
+        license      : String,
+        assetLicense : String,
+        description  : String,
+        video        : String,
+        date         : Date,
+        tags         : [String],
+        alias        : [String],
+        repository   : String,
+        gameWebsite  : String,
+        devWebsite   : String,
+        onlineplay   : Boolean
 
     }
 });
