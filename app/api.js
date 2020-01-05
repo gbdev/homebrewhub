230d8d0fbd8cfa59e39787b49dcae40a9ca2e518
Antonio Vivace <avivace4@gmail.com>
2020-01-05 14:00:16 +0100

    Remove /api/discord endpoint

--- app/api.js
+++ app/api.js
@@ -8,43 +8,11 @@ module.exports = function(app) {
         return !isNaN(value) && (function(x) { return (x | 0) === x; })(parseFloat(value))
     }
 
-    function countUsers(callback) {
-        var url = 'https://discordapp.com/api/guilds/303217943234215948/widget.json';
-        var count = 0
-        request.get({
-            url: url,
-            json: true,
-            headers: { 'User-Agent': 'request' }
-        }, (err, res, data) => {
-            if (err) {
-                console.log('Error:', err);
-                return (callback(null, err))
-            } else if (res.statusCode !== 200) {
-                console.log('Status:', res.statusCode);
-                return (callback(null, res.statusCode));
-            } else {
-                data.members.forEach(function(e) {
-                    count++
-                })
-                return (callback(count, false))
-            }
-        });
-    }
     app.use("/api", function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     });
-
-    // TODO: just import dmg01/discordBadge as npm module and include its route
-    app.get('/api/discord', function(req, res) {
-        countUsers(function(data, err) {
-            if (err) return res.send(err.toString())
-            var result = new Object()
-            result["count"] = data;
-            res.json(result)
-        })
-    })
 
     app.get('/api/info', function(req, res) {
         Game.countDocuments({}, function(err, gamecount) {
