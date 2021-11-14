eacbfd066edeaca4808f1814fd78a685d0a56934
Antonio Vivace <avivace4@gmail.com>
2021-11-15 00:54:36 +0100

    Allow specifying number of results

--- hhub/views.py
+++ hhub/views.py
@@ -6,22 +6,24 @@ import json
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.exceptions import FieldError
 
+
 def entry_manifest(request, pk):
     try:
         entry = Entry.objects.get(pk=pk)
     except Entry.DoesNotExist:
         return JsonResponse(
-            {'error': 'No entry found in the database with the given slug'}, status=404
+            {"error": "No entry found in the database with the given slug"}, status=404
         )
 
-    data = open(f'database/entries/{pk}/game.json').read()
+    data = open(f"database/entries/{pk}/game.json").read()
     jsonData = json.loads(data)
     return JsonResponse(jsonData)
 
 
 def entries_all(request):
-    sort_by_param = request.GET.get('sort', '')
-    order_by_param = request.GET.get('order_by', '')
+    sort_by_param = request.GET.get("sort", "")
+    order_by_param = request.GET.get("order_by", "")
+    num_elements = request.GET.get("results", 10)
 
     entries = Entry.objects.all()
 
@@ -30,12 +32,11 @@ def entries_all(request):
     try:
         entries = sort_and_order(entries, order_by_param, sort_by_param)
     except FieldError as e:
-        return JsonResponse(
-            {'error': str(e) }, status=400
-        )
+        return JsonResponse({"error": str(e)}, status=400)
 
-    paginator = Paginator(entries, 10)
-    page = request.GET.get('page', 1)
+    paginator = Paginator(entries, num_elements)
+    page = request.GET.get("page", 1)
+
     results = len(entries)
     try:
         entries = paginator.page(page)
@@ -50,15 +51,15 @@ def entries_all(request):
 
     json_entries = []
     for entry in entries:
-        data = open(f'database/entries/{entry.slug}/game.json').read()
+        data = open(f"database/entries/{entry.slug}/game.json").read()
         json_entries.append(json.loads(data))
     return JsonResponse(
         {
-            'results': results,
-            'page_total': paginator.num_pages,
-            'page_current': page,
-            'page_elements': len(serializer.data),
-            'entries': json_entries,
+            "results": results,
+            "page_total": paginator.num_pages,
+            "page_current": page,
+            "page_elements": len(serializer.data),
+            "entries": json_entries,
         },
         safe=False,
     )
@@ -67,15 +68,15 @@ def entries_all(request):
 
 def search_entries(request):
     # Catch query params
-    page = request.GET.get('page', '')
-    developer = request.GET.get('developer', '')
-    title = request.GET.get('title', '')
-    typetag = request.GET.get('typetag', '')
-    tags = request.GET.get('tags', '')
-    platform = request.GET.get('platform', '')
-    order_by_param = request.GET.get('order_by', '')
-    sort_by_param = request.GET.get('sort', '')
-    
+    page = request.GET.get("page", "")
+    developer = request.GET.get("developer", "")
+    title = request.GET.get("title", "")
+    typetag = request.GET.get("typetag", "")
+    tags = request.GET.get("tags", "")
+    platform = request.GET.get("platform", "")
+    order_by_param = request.GET.get("order_by", "")
+    sort_by_param = request.GET.get("sort", "")
+
     # Start selecting everything
     entries = Entry.objects.all()
 
@@ -110,15 +111,15 @@ def search_entries(request):
 
     json_entries = []
     for entry in entries:
-        data = open(f'database/entries/{entry.slug}/game.json').read()
+        data = open(f"database/entries/{entry.slug}/game.json").read()
         json_entries.append(json.loads(data))
     return JsonResponse(
         {
-            'results': results,
-            'page_total': paginator.num_pages,
-            'page_current': page,
-            'page_elements': len(serializer.data),
-            'entries': json_entries,
+            "results": results,
+            "page_total": paginator.num_pages,
+            "page_current": page,
+            "page_elements": len(serializer.data),
+            "entries": json_entries,
         },
         safe=False,
     )
@@ -128,21 +129,22 @@ def search_entries(request):
 def entry_platform(request, platform):
     return
 
+
 #########
 # UTILS #
 #########
 def sort_and_order(entries, col_name, sort_by_param):
     # regardless what user has submitted, we lowercase the input
-    col_name = col_name.lower().strip()  
+    col_name = col_name.lower().strip()
     sort_by_param = sort_by_param.lower().strip()
-    
+
     # if col_name has been specified and it is in allowed list of fields, check if sort has been specified
     if col_name in ["slug", "title"]:
         if sort_by_param in ["", "asc", "desc"]:
             if sort_by_param == "asc" or not sort_by_param:
                 return entries.order_by(col_name)
             elif sort_by_param == "desc":
-                # minus here means "desc" order, according to 
+                # minus here means "desc" order, according to
                 # https://docs.djangoproject.com/en/dev/ref/models/querysets/#order-by
                 return entries.order_by("-" + col_name)
         else:
@@ -152,8 +154,8 @@ def sort_and_order(entries, col_name, sort_by_param):
         if sort_by_param == "asc" or not sort_by_param:
             return entries.order_by("slug")
         elif sort_by_param == "desc":
-            # minus here means "desc" order, according to 
+            # minus here means "desc" order, according to
             # https://docs.djangoproject.com/en/dev/ref/models/querysets/#order-by
-            return entries.order_by("-slug") 
-    
+            return entries.order_by("-slug")
+
     return entries
